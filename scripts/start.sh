e249272f71ba7772281730cfa0091513a6b2d702
flz <francisco.lara@elca.ch>
2021-04-07 11:54:56 +0200

    added host reference

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,10 +4,13 @@ hostip=$(ip route show | awk '/default/ {print $3}')
 echo "$hostip   host.internal" >> /etc/hosts
 
 # Initiate MongoDB exporter
-prometheus-mongodb-exporter &
+#-mongodb.uri string 
+# Mongodb URI, format: [mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options] (default "mongodb://localhost:27017")
+
+prometheus-mongodb-exporter -mongodb.uri string mongodb://host.internal:27017 &
 
 # Initiate Squid exporter
-prometheus-squid-exporter &
+prometheus-squid-exporter -squid-hostname host.internal &
 
 # Initiate Telegraf
 telegraf &
